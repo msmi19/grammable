bd93499c36d012cce21cec204f0990601d00e116
Morgan Smith <morgansmith0219@gmail.com>
2019-09-19 01:30:03 +0000

    added create action

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require poppper
+//= require popper
 //= require tether
 //= require bootstrap-sprockets
 //= require_tree .
