ed44c924506f6fd0ce6f7b0c966dd4e914a8ec81
Morgan Smith <morgansmith0219@gmail.com>
2019-09-25 03:14:38 +0000

    heroku keeps giving error for app/config/environments/production.rb:2, updated this line, testing if it works

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,5 @@ module Grammable
     # the framework and any gems in your application.
   end
 end
+
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { 'grammable-morgan-smith.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'grammable-morgan-smith.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
